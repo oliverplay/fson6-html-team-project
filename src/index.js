24071e8d4fed54f5cdc6756a047f712055fa842f
Oleh Subotin <116948332+oliverplay@users.noreply.github.com>
2023-03-10 06:52:58 +0200

    qwe

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
-import './js/modal';
-import './js/modal';
-import './filmsListMarkup';
-import './js/pagination';
-import './js/getMoviesForKeyword';
-import './js/modalTeams';
-import './js/auth';
-import './js/scrollTop';
-import { fonNightDay } from './js/fonNightDay';
-import { modalFormLogin } from './js/modalFormLogin';
-import { initialLoader } from './js/loader';
-
-fonNightDay();
-modalFormLogin();
-
-initialLoader();
+// import './js/modal';
 
